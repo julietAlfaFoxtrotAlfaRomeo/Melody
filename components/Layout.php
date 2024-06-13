40bf9d3eb19e35c7821325dac3c275624593e2bc
julietAlfaFoxtrotAlfaRomeo <jafarshodik036@gmail.com>
2024-06-14 04:55:31 +0700

    first commit

---
+++ components/Layout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <?php include 'components/header.php'; ?>
+    <?php include 'components/main.php'; ?>
+    <?php include 'components/footer.php'; ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,1 @@
+<?php include 'components/Layout.php';
